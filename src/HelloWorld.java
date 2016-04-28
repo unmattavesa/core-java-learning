72b219e348c3acf342cf1552b134c9afe9006e2a
unmattavesa <unmattavesa*gmail.com>
2016-04-28 16:10:01 +0530

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+/**
+ * A Simple Hello World program
+ * 
+ * <pre>Usage :: 
+ *-----------------------------------------
+ * compile using :: javac HelloWorld.java
+ * 
+ * run using     :: java HelloWorld
+ *----------------------------------------- 
+ *</pre>
+ * @author unmattavesa
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		out.print("Hello World");
+		
+	}
+	
+}
